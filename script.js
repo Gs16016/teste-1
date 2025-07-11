425045b769a6ce1f035ef01690015b7325d6c511
Gs16016 <guroberto2010@gmail.com>
2025-07-11 12:17:22 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+const LINE_COUNT = 30;
+const MATRIX_TEXT = "teste ";
+const LOVE_CHARS = [""];
+
+function initMatrix() {
+  const container = document.getElementById('terminal');
+  for (let i = 0; i < LINE_COUNT; i++) {
+    const line = document.createElement('div');
+    line.className = 'text-container';
+    line.style.ani(line);
+  }
+}
+mation = `scroll ${5 + i / 2}s linear infinite`;
+    line.style.animationDelay = `${i * 0.3}s`;
+    line.textContent = MATRIX_TEXT.repeat(window.innerWidth / 50);
+    container.appendChild
+initMatrix();
